9e5681187b60297282fcf30f6583bb8e0e578613
Matheus Vinicius <57428641+matheus778@users.noreply.github.com>
2023-02-11 12:35:36 -0300

    projeto finalizado

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const valueDays = document.querySelectorAll('.value-day');
+
+async function getDataJson() {
+  const dataFile = await fetch('../data.json');
+  return await dataFile.json();
+}
+
+  valueDays.forEach((el, index) => {
+    let detailHover  = document.createElement('div');
+    detailHover.classList.add('detail-hover');
+
+    getDataJson()
+      .then(data =>{
+        let amount = data[index].amount;
+        detailHover.textContent = `R$ ${amount}`;
+
+        let heightValue = amount * 2.5;
+        el.style.height = `${heightValue}px`
+
+      })
+
+    el.addEventListener('mouseout', ()=> {
+      el.parentElement.removeChild(detailHover)
+    })
+
+    el.addEventListener('mouseover', ()=> {
+      
+      el.parentElement.appendChild(detailHover);
+    })
+
+    el.addEventListener('click', () => {
+      el.classList.toggle('clicked')
+    })
+
+  })
+
